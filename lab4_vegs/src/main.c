8decc052108ffe99296b35e1b2820034dcb7d09c
Верзунов Сергей <verzunov@hotmail.com>
2023-10-17 11:37:08 +0600

    Добавлен метод Вегстейна

--- lab4_vegs/src/main.c
+++ lab4_vegs/src/main.c
@@ -1,34 +1,38 @@
 #define EPS 1e-6
 #define MAX_ITER 100
-#define H 1e-5
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 double x0;
 double x1;
+double x2;
 double f(double x)
 {
     return x*x -4 ;
 }
-double psi(double x)
+double psi(double x0, double x1)
 {
-    
-    return -1.0/((f(x+H)-f(x))/H);
+    return -1.0/(
+        (f(x1)-f(x0))/(x1-x0)
+    ); /*(3)*/
 }
 int main()
 {
-    printf("Введите начальное приближение\n");
+    printf("Введите 1 начальное приближение\n");
     scanf("%lf", &x1);
+    printf("Введите 2 начальное приближение\n");
+    scanf("%lf", &x2);
     int i;
     for(i=0; i<MAX_ITER;i++)
     {
         x0=x1;
+        x1=x2;
         double delta;
-        delta=f(x0)*psi(x0);
+        delta=f(x1)*psi(x0, x1);
         if (fabs(delta)<EPS) break;
-        x1=x0+delta;
-        printf("Текущее приближение x=%lf\n",x1);
+        x2=x1+delta;
+        printf("Текущее приближение x2=%lf\n",x2);
     }
     if (i==MAX_ITER)
     {
