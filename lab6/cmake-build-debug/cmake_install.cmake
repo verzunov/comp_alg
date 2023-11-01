d272235e8d52c427f7b777260b8689d29cc461ca
Верзунов Сергей <verzunov@hotmail.com>
2023-11-01 08:39:38 +0600

    Полином Лагранжа

--- lab6/cmake-build-debug/cmake_install.cmake
+++ lab6/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/sverzunov/git/comp_alg/untitled
+# Install script for directory: /home/sverzunov/git/comp_alg/lagranj
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/sverzunov/git/comp_alg/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/sverzunov/git/comp_alg/lagranj/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- lab6/main.c
+++ lab6/main.c
@@ -1,67 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-void intro(double *x, double *y, double xxs, double *yxs, int q){
-    int w,d;
-    double k;
-
-    for(d=0; d<q; d++){
-        k=1;
-        for(w=0; w<q; w++){
-            if(w==d) continue;
-            k*=(xxs-x[w])/(x[d]-x[w]);
-        }
-        *yxs+=y[d]*k;
+#define MAX_NODE 10
+double X[MAX_NODE], Y[MAX_NODE];
+int N;
+FILE *fifa;
+double l_(int i, double x)
+{
+    double acc =1.0;
+    for(int j=0; j<N; j++)
+    {
+        if (i == j) continue;
+        acc *= x - X[j];
     }
+    return acc;
 }
+double l(int i, double x)
+{
+    return l_(i, x)/l_(i, X[i]);
+}
+double P(double x)
+{
+    double acc=0;
+    for(int i=0; i<N; i++)
+        acc+=Y[i]*l(i,x);
+    return  acc;
 
+}
 int main() {
-    int i, m;
-    FILE *fifa;
-    double *X, *Y, j = 0, Xcsy, Ycsy = 0;
-    printf("Input number of knots");
-    scanf("%d", &m);
-
-    X = malloc(m * sizeof(double));
-    Y = malloc(m * sizeof(double));
-
-    printf("Input Xi:\n");
-    for (i = 0; i < m; i++) {
-        scanf("%lf", &X[i]);
+    printf("Введите количество узлов ");
+    scanf("%d",&N);
+    for(int i=0; i<N; i++)
+    {
+        printf("Введите x[%d] y[%d] ", i, i);
+        scanf("%lf %lf", &X[i], &Y[i]);
     }
-    printf("Input Yi:\n");
-    for (i = 0; i < m; i++) {
-        scanf("%lf", &Y[i]);
-    }
-    printf("Input Xcsy:\n");
-    scanf("%lf", &Xcsy);
-    intro(X, Y, Xcsy, &Ycsy, m);
-    printf("Ycsy = %lf", Ycsy);
+    double x;
+    printf ("Введите значение x ");
+    scanf("%lf", &x);
+    double y =P(x);
+    printf("P=%lf" , y);
     fifa = popen("gnuplot", "w");
     fprintf(fifa, "set grid\n");
-    fprintf(fifa, "set xrange [0:2.5]\n");
-    fprintf(fifa, "set yrange [0:1]\n");
+    fprintf(fifa, "set xrange [-5:5]\n");
+    fprintf(fifa, "set yrange [0:10]\n");
     fprintf(fifa, "set style line 1 lc rgb 'red' pt 2\n");
     fprintf(fifa, "set style line 2 lc rgb 'blue' pt 7\n");
     fprintf(fifa, "plot '-' w p ls 1 title '', '-' with lines title '', '-' w p ls 2 title ''\n");
-    fprintf(fifa, "%lf %lf\n", Xcsy, Ycsy);
+    fprintf(fifa, "%lf %lf\n", x, y);
     fprintf(fifa, "e\n");
 
-    while (j <= 3) {
-        Ycsy = 0;
-        intro(X, Y, j, &Ycsy, m);
-        fprintf(fifa, "%lf %lf\n", j, Ycsy);
-        j += 0.01;
+
+    double a=-5;
+    while (a <= 5) {
+
+        fprintf(fifa, "%lf %lf\n", a, P(a));
+       a += 0.01;
     }
     fprintf(fifa, "e\n");
-    for (i = 0; i < m; i++) {
+    for (int i = 0; i < N; i++) {
         fprintf(fifa, "%lf %lf\n", X[i], Y[i]);
     }
+    fprintf(fifa, "set title \"Polinom Lagranja\", font '20'\n");
     fprintf(fifa, "e\n");
     fprintf(fifa, "pause mouse keypress\n");
     fflush(fifa);
-
-    free(X);
-    free(Y);
     return 0;
 }
